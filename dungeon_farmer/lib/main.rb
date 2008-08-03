51983a8dbf1e2b56b77e8c20f19e23cfcb4f54c8
Daniel X Moore <yahivin@gmail.com>
2008-08-03 00:57:19 +0000

    Removed RMagick dependency.

--- dungeon_farmer/lib/main.rb
+++ dungeon_farmer/lib/main.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'RMagick'
 require 'gosu'
 
 $:.unshift File.dirname(__FILE__) + '/../gratr-0.4.3/lib'
@@ -7,7 +6,7 @@ require 'gratr.rb'
 require 'gratr/import'
 
 lib = File.dirname(__FILE__) + '/'
-files = %w[graphical observable item prize seed fruit game_entity plant area creature animals player goblin cell diamond_square image_maker image_loader game_window random task manager]
+files = %w[graphical observable item prize seed fruit game_entity plant area creature animals player goblin cell diamond_square image_loader game_window random task manager]
 files.each {|f| require lib+"#{f}.rb" }
 
 if __FILE__ == $0
