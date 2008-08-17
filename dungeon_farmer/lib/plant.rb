1b38749978088200203ac4d061643abb995e286c
Daniel X Moore <yahivin@gmail.com>
2008-08-17 22:54:37 +0000

    Refactored plant functions

--- dungeon_farmer/lib/plant.rb
+++ dungeon_farmer/lib/plant.rb
@@ -8,36 +8,39 @@ class Plant < GameEntity
     @@images ||= ['seed.png', 'sprout.png', 'plant1.png', 'plant2.png', 'plant3.png'].map do
       |file| il(file)
     end
+    
+    @maturity_age = 200
+    @death_age = 350
+    @dead = false
   end
     
   def update
     @age += 1
     
-    if @age % 2 == 0 && mature?
-      if rand(73) == 0
-        fruit_cell = [@cell, @cell, @cell, @cell.north, @cell.south, @cell.east, @cell.west].random
-        fruit_cell << Fruit.new
-        notify(:fruit, fruit_cell)
+    unless @dead
+      if @age % 2 == 0 && mature?
+        if rand(73) == 0
+          fruit_cell = [@cell, @cell, @cell, @cell.north, @cell.south, @cell.east, @cell.west].random
+          fruit_cell << Fruit.new
+          notify(:fruit, fruit_cell)
+        end
       end
-    end
-    
-    if dead?
-      rand(4).times do
-        seed_cell = @cell.neighbours.random
-        seed_cell.seeds += 1
-        notify(:seed, seed_cell)
+
+      if @age >= @death_age
+        die
       end
-      remove
     end
   end
   
   def remove
-    @cell.delete self
-    @area.remove_entity self
+    @cell.delete self if @cell
+    @area.remove_entity self if @area
+    @cell = nil
+    @area = nil
   end
   
   def mature?
-    @age > 199
+    @age >= @maturity_age
   end
   
   def obstructs?
@@ -45,7 +48,7 @@ class Plant < GameEntity
   end
   
   def dead?
-    @age > 349
+    @dead
   end
   
   def image
@@ -54,5 +57,18 @@ class Plant < GameEntity
   
   def can_pick_up?
     false
+  end
+  
+  def die
+    unless @dead
+      rand(4).times do
+        seed_cell = @cell.neighbours.random
+        seed_cell.seeds += 1
+        notify(:seed, seed_cell)
+      end
+      remove
+    end
+    
+    @dead = true
   end
 end
