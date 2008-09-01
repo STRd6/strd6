827076d41e3785946f5379946fbea4b9fb2b70f1
Daniel X Moore <yahivin@gmail.com>
2008-09-01 23:11:21 +0000

    Generated authentication and installed rspec plugin.

--- boomtime/app/controllers/application.rb
+++ boomtime/app/controllers/application.rb
@@ -2,8 +2,10 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include AuthenticatedSystem
+  
   helper :all # include all helpers, all the time
-
+  
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '092f8ea65f75723641cd47fbcb74608d'
--- boomtime/app/helpers/application_helper.rb
+++ boomtime/app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def p_label(label, data)
+    "<p>#{label_tag label}: <br/>
+     #{data}</p>"
+  end
+  
 end
