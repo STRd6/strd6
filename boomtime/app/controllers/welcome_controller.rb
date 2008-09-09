b992f80f5fdfa56d108263a991c2f7ec54e9ff51
Daniel X Moore <yahivin@gmail.com>
2008-09-09 05:26:01 +0000

    Lan un-effed the sass a bit. Also I added a UI Test page, accessable at welcome/ui_test

--- boomtime/app/controllers/welcome_controller.rb
+++ boomtime/app/controllers/welcome_controller.rb
@@ -2,4 +2,13 @@ class WelcomeController < ApplicationController
   def index
     
   end
+  
+  def ui_test
+    @title = "A Fabulous UI Test!"
+    flash[:notice] = "The world's best notice"
+    flash[:error] = "A terrifying error!!"
+    
+    @user = User.first
+    @user.errors.add "A really uncool error"
+  end
 end
