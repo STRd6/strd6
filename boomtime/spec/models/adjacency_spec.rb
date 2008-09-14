5da17e7daf14f05ff63dc7d71a739c4aa573b74d
Daniel X Moore <yahivin@gmail.com>
2008-09-14 00:49:14 +0000

    Speccing the crap out of adjacency.

--- boomtime/spec/models/adjacency_spec.rb
+++ boomtime/spec/models/adjacency_spec.rb
@@ -6,4 +6,10 @@ describe Adjacency do
     a.area.should_not be_nil
     a.adjacent_area.should_not be_nil
   end
+  
+  it "should be invalid if area or adjacenct_area is nil" do
+    Factory.build(:adjacency, :area => nil).should_not be_valid
+    Factory.build(:adjacency, :adjacent_area => nil).should_not be_valid
+  end
+  
 end
