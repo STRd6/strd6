f45c261979023310fbcb118781e4e1fbfc75d78d
Daniel X Moore <yahivin@gmail.com>
2008-09-27 02:46:56 +0000

    Added beginnings of drag'n drop.

--- boomtime/public/javascripts/application.js
+++ boomtime/public/javascripts/application.js
@@ -1,6 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+// Window Dragon
 function end_drag(element) {
   raise(element);
   store_position(element);
@@ -22,4 +23,10 @@ function store_position(element) {
 
 function raise(element) {
   
+}
+
+// Item/Inventory Dragon Drop
+
+function item_dropped(item, drop) {
+  drop.insert(item.remove());
 }
