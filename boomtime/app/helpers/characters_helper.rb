a0f00d76839b71c22466b3ca39eb81359687d37d
Daniel X Moore <yahivin@gmail.com>
2008-09-27 15:41:21 +0000

    Added icon_for helper function.

--- boomtime/app/helpers/characters_helper.rb
+++ boomtime/app/helpers/characters_helper.rb
@@ -1,2 +1,7 @@
 module CharactersHelper
+  ICOMAP = {:str => 'stars', :dex => 'red_black', :pow => 'flame_lamp'}
+  
+  def icon_for(key)
+    image_tag "icons/#{ICOMAP[key]}.png", :class => 'icon'
+  end
 end
