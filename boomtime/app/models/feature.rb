e3375e888e240e981a0bf0470324dcdd7de8d2fc
Daniel X Moore <yahivin@gmail.com>
2008-10-10 02:29:22 +0000

    Displaying the appropriate image for features.

--- boomtime/app/models/feature.rb
+++ boomtime/app/models/feature.rb
@@ -1,13 +1,23 @@
 class Feature < ActiveRecord::Base
   belongs_to :area
   
+  # VV Display Datum Party Bus VV #
   def update_position(left, top)
     self.left = left.to_i
     self.top = top.to_i
     save
   end
   
+  def image
+    if img = super
+      return "plants/#{img}"
+    else
+      return "plants/tree3"
+    end
+  end
+  
   def overlay_text
     false
   end
+  # ^^ Display Datum Party Bus ^^ #
 end
