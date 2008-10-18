73f7c12d33a9525d17ade24d2242481f82f8ff60
Daniel X Moore <yahivin@gmail.com>
2008-10-18 21:28:24 +0000

    Added logged in tests to application layout spec, fixed incorrect render call using new displayable helper.

--- boomtime/spec/views/layouts/application.html_spec.rb
+++ boomtime/spec/views/layouts/application.html_spec.rb
@@ -9,5 +9,25 @@ describe "/layouts/application.html.haml" do
   it "should render application layout" do
     render "/layouts/application"
   end
+  
+  describe "while logged in" do
+    before(:each) do
+       @controller.template.stub!(:current_user).and_return(Factory(:user))
+    end
+
+    it "should render application layout" do
+      render "/layouts/application"
+    end
+
+    describe "with an active character" do
+      before(:each) do
+         @controller.template.stub!(:active_character).and_return(Factory(:character))
+      end
+
+      it "should render application layout" do
+        render "/layouts/application"
+      end
+    end
+  end
 end
 
