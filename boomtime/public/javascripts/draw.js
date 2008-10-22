ae124d2edc9e6ea33bd28aa7097944cf0a4c116f
Daniel X Moore <yahivin@gmail.com>
2008-10-22 07:09:36 +0000

    Classitated the pixel editor, still way way crude though.

---
+++ boomtime/public/javascripts/draw.js
@@ -0,0 +1,109 @@
+var Pixel = Class.create({
+  initialize: function(element, canvas) {
+    this.element = $(element);
+    this.canvas = canvas;
+    this.element.observe('mousemove', this.mousemove.bindAsEventListener(this));
+    //this.observe('mouseup', this.mouseup.bindAsEventListener(this));
+    this.element.observe('mousedown', this.mousedown.bindAsEventListener(this));
+  },
+  
+  mousedown: function(event) { 
+    this.canvas.tool.mousedown(event);
+  },
+  mouseup: function(event) {  },
+  
+  mousemove: function(event) { 
+    this.canvas.tool.mousemove(event);
+  }  
+});
+
+var Tool = Class.create({
+  initialize: function() {  },  
+  mousedown: function(event) {  },
+  mouseup: function(event) {  },
+  mousemove: function(event) {  },
+  canvasout: function(event) {  }
+});
+
+var EyeDropper = Class.create(Tool, {
+  mousedown: function(event) {
+    //alert('Pre-parse: ' + event.element().style.backgroundColor);
+    //alert(this.parseColor(event.element().style.backgroundColor));
+    color = this.parseColor(event.element().style.backgroundColor) || 'FFFFFF';
+    $('current_color').value = color;
+    $('current_color').onblur();
+  },
+  
+  parseColor: function(colorString) {
+    if(!colorString) {
+      return false
+    }
+    
+    var bits = /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/.exec(colorString);
+    return [
+      this.toHex(bits[1]),
+      this.toHex(bits[2]),
+      this.toHex(bits[3])
+    ].join('').toUpperCase();
+  },  
+  toHex: function(bits) {
+    s = parseInt(bits).toString(16);
+    if(s.length == 1) {
+      s = '0' + s
+    }
+    return s;
+  }
+});
+
+var dropper = new EyeDropper();
+
+var Pencil = Class.create(Tool, {
+  initialize: function() {
+  },
+  
+  mousedown: function(event) {
+    this.active = true;
+  },
+  
+  mouseup: function(event) {
+    this.active = false;
+  },
+  
+  mousemove: function(event) {
+    if(this.active) {
+      event.element().style.backgroundColor = '#' + $F('current_color');
+    }
+  },
+  
+  canvasout: function(event) {
+    this.active = false;
+  }
+});
+
+var pencil = new Pencil();
+
+var Canvas = Class.create({
+  initialize: function(element) {
+    this.element = $(element);
+    //this.element.observe('mousedown', this.mousedown);
+    this.element.observe('mouseup', this.mouseup.bindAsEventListener(this));
+    this.element.observe('mouseout', this.mouseout.bindAsEventListener(this));
+    this.tool = pencil;
+  },
+  
+  mousedown: function(event) {
+    
+  },
+  
+  mouseup: function(event) {
+    this.tool.mouseup(event);
+  },
+  
+  mouseout: function(event) {
+    //this.tool.canvasout(event);
+  },
+  
+  setTool: function(tool) {
+    this.tool = tool;
+  }  
+});
