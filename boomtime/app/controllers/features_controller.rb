de577a461b6a25a908ce2c0595188d4adde01e6f
Daniel X Moore <yahivin@gmail.com>
2008-10-22 01:42:59 +0000

    Features have a very basic form of protyping.

--- boomtime/app/controllers/features_controller.rb
+++ boomtime/app/controllers/features_controller.rb
@@ -1,13 +1,3 @@
 class FeaturesController < ResourceController::Base
   include ObjectController
-  
-  create.before do
-    object.creator = active_character
-    object.area = active_character.area if active_character
-    if params[:feature][:properties]
-      logger.info params[:feature][:properties]
-      object.properties = {:image => params[:feature][:properties][:image]}
-      logger.info("OBJ: #{object.properties}")
-    end
-  end
 end
--- boomtime/app/models/feature.rb
+++ boomtime/app/models/feature.rb
@@ -8,8 +8,26 @@ class Feature < ActiveRecord::Base
   
   before_create :initialize_properties
   
+  Proteus = {
+    :tree => {:image => 'plants/tree1'},
+    :bush => {:image => 'plants/bush3'}
+  }
+  
   def initialize_properties
     self.properties ||= {}
+  end
+  
+  def prototype=(prototype)
+    self.properties ||= {}
+    
+    if prototype
+      @prototype = prototype.to_sym
+      self.properties.merge! Proteus[@prototype] if Proteus[@prototype]
+    end
+  end
+  
+  def prototype
+    @prototype
   end
   
   def image
