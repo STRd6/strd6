7740b3456d235261d72551817693c469d804de79
Daniel X Moore <yahivin@gmail.com>
2008-11-07 00:03:02 +0000

    Game updates events instead of chat log for items, features and characters.

--- boomtime/app/controllers/game_controller.rb
+++ boomtime/app/controllers/game_controller.rb
@@ -34,7 +34,7 @@ class GameController < ApplicationController
       element.update_position(params[:left], params[:top])
       # Update all clients in area
       render_to_area area do |page|
-        page.call :add_chat, "#{h current_user} has moved a #{klass}!"
+        page.call :addEvent, "#{h current_user} has moved a #{klass}!"
         page.call 'game.updateDisplayable', element.css_id, element.left, element.top
       end
       render :nothing => true
@@ -72,7 +72,7 @@ class GameController < ApplicationController
           prev_item.save
           
           render_to_area area do |page|
-            page.call :add_chat, "#{h current_user} has dropped #{h prev_item.name}!"
+            page.call :addEvent, "#{h current_user} has dropped #{h prev_item.name}!"
             page.call 'game.updateDisplayable', prev_item.css_id, prev_item.left, prev_item.top
           end
         end
@@ -82,7 +82,7 @@ class GameController < ApplicationController
         item.save
         # Update all clients in area
         render_to_area area do |page|
-          page.call :add_chat, "#{h active_character.name} has picked up #{h item.name}!"
+          page.call :addEvent, "#{h active_character.name} has picked up #{h item.name}!"
           page.call :got_item, item.css_id, active_character.id
         end
       end
@@ -98,7 +98,7 @@ class GameController < ApplicationController
     # Add pile resources to player
     if active_character.get_pile(item)
       render_to_area active_character.area do |page|
-        page.call :add_chat, "#{h active_character.name} has picked up #{h item.name}!"
+        page.call :addEvent, "#{h active_character.name} has picked up #{h item.name}!"
         page.call :got_item, item.css_id, active_character.id
       end
       render :update do |page|
@@ -136,7 +136,7 @@ class GameController < ApplicationController
     if area
       if old_area = active_character.area
         render_to_area old_area do |page|
-          page.call :add_chat, "#{h active_character.name} has left the area."
+          page.call :addEvent, "#{h active_character.name} has left the area."
           page.call 'game.removeDisplayable', active_character.css_id
         end
       end
@@ -145,7 +145,7 @@ class GameController < ApplicationController
       active_character.save
       
       render_to_area area do |page|
-        page.call :add_chat, "#{h active_character.name} has entered the area."
+        page.call :addEvent, "#{h active_character.name} has entered the area."
         page.call 'game.updateDisplayable',
           active_character.css_id, active_character.left, active_character.top
       end
@@ -161,7 +161,7 @@ class GameController < ApplicationController
 
       if new_item
         render_to_area active_character.area do |page|
-          page.call :add_chat, "#{h active_character.name} chopped some wood."
+          page.call :addEvent, "#{h active_character.name} chopped some wood."
           page.call 'game.removeDisplayable', target.css_id
           page.call 'game.updateDisplayable',
             new_item.css_id, new_item.left, new_item.top
