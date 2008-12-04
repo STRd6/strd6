da875b2e544a958a1c15857e1612a1822dcb4e68
Daniel X Moore <yahivin@gmail.com>
2008-12-04 06:22:14 +0000

    Added and configured exception notifier.

---
+++ hero_squad/config/initializers/exception_notifier.rb
@@ -0,0 +1,3 @@
+ExceptionNotifier.exception_recipients = %w[yahivin@gmail.com]
+ExceptionNotifier.sender_address = %["HeroSquad Error" <herosquad.error@strd6.com>]
+ExceptionNotifier.email_prefix = "[HEROSQUAD:ERROR] " 
