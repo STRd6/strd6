8b4df66b421d5795ba9e1fb00aacc91420fb539d
Daniel X Moore <yahivin@gmail.com>
2009-01-11 23:47:59 +0000

    Changed max_hp/en to hp/en_max

--- hero_squad/db/migrate/20081122033628_create_characters.rb
+++ hero_squad/db/migrate/20081122033628_create_characters.rb
@@ -12,32 +12,32 @@ class CreateCharacters < ActiveRecord::Migration
     
     Character.create({
       :name => "Cleric",
-      :base_stats => {:str => 5, :dex => 5, :pow => 5, :max_hp => 50, :max_en => 40, :move => 2}
+      :base_stats => {:str => 5, :dex => 5, :pow => 5, :hp_max => 50, :en_max => 40, :move => 2}
     })
     
     Character.create({
       :name => "Fighter",
-      :base_stats => {:str => 6, :dex => 5, :pow => 4, :max_hp => 50, :max_en => 30, :move => 3}
+      :base_stats => {:str => 6, :dex => 5, :pow => 4, :hp_max => 50, :en_max => 30, :move => 3}
     })
     
     Character.create({
       :name => "Gladiator",
-      :base_stats => {:str => 7, :dex => 4, :pow => 3, :max_hp => 60, :max_en => 20, :move => 2}
+      :base_stats => {:str => 7, :dex => 4, :pow => 3, :hp_max => 60, :en_max => 20, :move => 2}
     })
     
     Character.create({
       :name => "Mage",
-      :base_stats => {:str => 4, :dex => 5, :pow => 6, :max_hp => 40, :max_en => 50, :move => 3}
+      :base_stats => {:str => 4, :dex => 5, :pow => 6, :hp_max => 40, :en_max => 50, :move => 3}
     })
     
     Character.create({
       :name => "Theif",
-      :base_stats => {:str => 5, :dex => 6, :pow => 4, :max_hp => 40, :max_en => 40, :move => 3}
+      :base_stats => {:str => 5, :dex => 6, :pow => 4, :hp_max => 40, :en_max => 40, :move => 3}
     })
     
     Character.create({
       :name => "Wizard",
-      :base_stats => {:str => 3, :dex => 3, :pow => 7, :max_hp => 30, :max_en => 60, :move => 2}
+      :base_stats => {:str => 3, :dex => 3, :pow => 7, :hp_max => 30, :en_max => 60, :move => 2}
     })
   end
 
