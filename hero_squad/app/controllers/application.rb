26db902870ef76cbcb646bc1166e53ce4e6edf6c
Daniel X Moore <yahivin@gmail.com>
2009-01-11 23:44:24 +0000

    Don't use all helpers at all times.

--- hero_squad/app/controllers/application.rb
+++ hero_squad/app/controllers/application.rb
@@ -2,8 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '01af6afa7f8f56bfa759ae004d49b701'
