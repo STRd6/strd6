6969d8bf3980f45a6640e5e035d19dc4f8ce603f
Daniel X Moore <yahivin@gmail.com>
2009-01-12 01:01:35 +0000

    Added attribute and helper to render.

--- hero_squad/app/helpers/application_helper.rb
+++ hero_squad/app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def draw_attribute(name, value)
+    render :partial => "character_instances/attribute", :locals => {
+      :name => name,
+      :value => value,
+    }
+  end
 end
