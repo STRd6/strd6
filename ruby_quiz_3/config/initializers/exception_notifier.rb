e6e1a089c766690e59b5a12b758834f4d299d549
Daniel X Moore <yahivin@gmail.com>
2009-01-18 18:41:59 +0000

    Added exeption notifier.

---
+++ ruby_quiz_3/config/initializers/exception_notifier.rb
@@ -0,0 +1,3 @@
+ExceptionNotifier.exception_recipients = %w[yahivin@gmail.com]
+ExceptionNotifier.sender_address = %["Ruby Quiz 3 Error" <rubyquiz3.error@strd6.com>]
+ExceptionNotifier.email_prefix = "[RUBY QUIZ 3:ERROR] " 
