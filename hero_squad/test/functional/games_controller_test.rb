96c980a2a459ecc9bac039f42033f5e8a30fbf73
Daniel X Moore <yahivin@gmail.com>
2009-02-01 19:00:24 +0000

    Updated games controller test.

--- hero_squad/test/functional/games_controller_test.rb
+++ hero_squad/test/functional/games_controller_test.rb
@@ -1,48 +1,51 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  context "on CREATE" do
-    setup do
-      Factory :player
-      Factory :player
-      
-      post :create, :players => [1,2], :name => 'Cool game name'
-    end
-    
-    should_respond_with :redirect
-  end
-  
-  context "on INDEX" do
+  context "Games Controller" do
     setup do
       ActionView::Base.any_instance.stubs(:juggernaut)
-      Factory :game
-      
-      get :index
     end
     
-    should_respond_with :success
-  end
-  
-  context "on SHOW" do
-    setup do
-      ActionView::Base.any_instance.stubs(:juggernaut)
-      g = Factory :game
-      
-      get :show, :id => g.id
+    context "on CREATE" do
+      setup do
+        Factory :player
+        Factory :player
+
+        post :create, :players => [1,2], :name => 'Cool game name'
+      end
+
+      should_respond_with :redirect
     end
-    
-    should_respond_with :success
-    should_assign_to :game
-  end
-  
-  context "on NEW" do
-    setup do
-      ActionView::Base.any_instance.stubs(:juggernaut)
-      get :new
+
+    context "on INDEX" do
+      setup do
+        Factory :game
+
+        get :index
+      end
+
+      should_respond_with :success
     end
-    
-    should_respond_with :success
-    should_assign_to :game
-    should_render_a_form
+
+    context "on SHOW" do
+      setup do
+        game = Factory :game
+
+        get :show, :id => game.id
+      end
+
+      should_respond_with :success
+      should_assign_to :game
+    end
+
+    context "on NEW" do
+      setup do
+        get :new
+      end
+
+      should_respond_with :success
+      should_assign_to :game
+      should_render_a_form
+    end
   end
 end
