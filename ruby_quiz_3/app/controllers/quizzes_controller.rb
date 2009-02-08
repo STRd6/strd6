6259464f3ad455dcce8d5e95989417837ef3abc0
Daniel X Moore <yahivin@gmail.com>
2009-02-08 19:28:07 +0000

    Simplified loading of last quiz when no id is given.

--- ruby_quiz_3/app/controllers/quizzes_controller.rb
+++ ruby_quiz_3/app/controllers/quizzes_controller.rb
@@ -1,17 +1,4 @@
 class QuizzesController < ResourceController::Base
   actions :show, :index
-  
-  caches_page :index, :show
-  
-  before_filter :authenticate, :except => [ :index , :show ]
-  
-  private
-  
-  def object
-    if params[:id]
-      Quiz.find(params[:id])
-    else
-      Quiz.last
-    end
-  end
+  caches_page :show, :index
 end
