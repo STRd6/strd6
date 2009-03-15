962e3d74db181f0c768c8f084a6b85f4053af71c
Daniel X Moore <yahivin@gmail.com>
2009-03-15 02:42:17 +0000

    Added OpenId plugin

--- hero_squad/db/schema.rb
+++ hero_squad/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090128032024) do
+ActiveRecord::Schema.define(:version => 20090315023011) do
 
   create_table "abilities", :force => true do |t|
     t.string   "name",                                     :null => false
@@ -99,6 +99,21 @@ ActiveRecord::Schema.define(:version => 20090128032024) do
   end
 
   add_index "items", ["name"], :name => "index_items_on_name", :unique => true
+
+  create_table "open_id_authentication_associations", :force => true do |t|
+    t.integer "issued"
+    t.integer "lifetime"
+    t.string  "handle"
+    t.string  "assoc_type"
+    t.binary  "server_url"
+    t.binary  "secret"
+  end
+
+  create_table "open_id_authentication_nonces", :force => true do |t|
+    t.integer "timestamp",  :null => false
+    t.string  "server_url"
+    t.string  "salt",       :null => false
+  end
 
   create_table "players", :force => true do |t|
     t.string   "name",       :null => false
