40de969c73de4b9dd5d19ea443d1c03464915dc8
Daniel X Moore <yahivin@gmail.com>
2009-03-25 06:22:19 +0000

    Improved page titles

--- ruby_quiz_3/app/controllers/quizzes_controller.rb
+++ ruby_quiz_3/app/controllers/quizzes_controller.rb
@@ -2,4 +2,9 @@ class QuizzesController < ResourceController::Base
   caches_page :show, :index
   
   before_filter :authenticate, :except => [ :show, :index ]
+  
+  show.before do
+    @quiz = Quiz.last unless @quiz
+    @title = "#{@quiz.title} (#{@quiz.id})"
+  end
 end
