95358561b621ded7d74a40ebb331682c794eae1c
Daniel X Moore <yahivin@gmail.com>
2009-03-28 05:05:04 +0000

    Moved CSS Added controller, layout
    Started Authenticator plugin

---
+++ boomhammer/vendor/plugins/the_authenticator/config/routes.rb
@@ -0,0 +1,8 @@
+ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'accounts', :action => 'create'
+  map.signup '/signup', :controller => 'accounts', :action => 'new'
+
+  map.open_id_complete 'session', :controller => "sessions", :action => "create", :requirements => { :method => :get }
+end
---
+++ boomhammer/vendor/plugins/the_authenticator/tasks/the_authenticator_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :the_authenticator do
+#   # Task goes here
+# end
