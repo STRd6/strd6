0363b0e4d2eedbef2fe77871bec834136dbce677
Daniel X Moore <yahivin@gmail.com>
2009-03-29 18:25:43 +0000

    Redirecting to the next appropriate action when going to game index.

--- boomhammer/app/controllers/game_controller.rb
+++ boomhammer/app/controllers/game_controller.rb
@@ -1,2 +1,17 @@
 class GameController < ApplicationController
+  def index
+    @title = "Welcome"
+
+    if logged_in?
+      if current_character
+        redirect_to current_character.area
+      elsif current_account_characters.size > 0
+        redirect_to characters_path
+      else
+        redirect_to new_character_path
+      end
+    else
+      render :action => :welcome
+    end
+  end
 end
