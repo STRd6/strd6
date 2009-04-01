05a6cc4a84a5ce6e615c6690316a8d9df2f81e28
Daniel X Moore <yahivin@gmail.com>
2009-04-01 05:17:14 +0000

    Added crude image selector.

--- boomhammer/app/helpers/images_helper.rb
+++ boomhammer/app/helpers/images_helper.rb
@@ -6,4 +6,11 @@ module ImagesHelper
   def draw_badge(badge)
     image_tag "/images/badges/#{badge.image_file_name}", :alt => badge, :title => badge
   end
+
+  def image_select(form)
+    Image.all.map do |image|
+      form.label("image_id_#{image.id}", image_for(image) ) +
+      form.radio_button(:image_id, image.id)
+    end
+  end
 end
