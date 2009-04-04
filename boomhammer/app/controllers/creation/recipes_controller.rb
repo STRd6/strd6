b636d254ccafae2edf1faa3f45753b55f0c3ad27
Daniel X Moore <yahivin@gmail.com>
2009-04-04 04:46:03 +0000

    Recipes is totally pimps.

--- boomhammer/app/controllers/creation/recipes_controller.rb
+++ boomhammer/app/controllers/creation/recipes_controller.rb
@@ -4,4 +4,15 @@ class Creation::RecipesController < ResourceController::Base
   show.before do
     @title = @recipe.name
   end
+
+  create.before do
+    # HAX: Nested assignment doesn't link these mofos up
+    @recipe.recipe_components.each do |ingredient|
+      ingredient.recipe = @recipe
+    end
+    # HAX: Nested assignment doesn't link these mofos up
+    @recipe.recipe_outcomes.each do |outcome|
+      outcome.recipe = @recipe
+    end
+  end
 end
