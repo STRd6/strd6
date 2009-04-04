1aa2937455b417883e179b2e4db9c0110a4f9ba0
Daniel X Moore <yahivin@gmail.com>
2009-04-04 22:08:10 +0000

    Adding methods to loot

--- boomhammer/app/models/loot.rb
+++ boomhammer/app/models/loot.rb
@@ -1,7 +1,13 @@
 class Loot < ActiveRecord::Base
+  include Named
+
   belongs_to :opportunity_base
   belongs_to :item_base
 
   validates_presence_of :opportunity_base
   validates_presence_of :item_base
+  validates_numericality_of :weight, :greater_than => 0
+
+  delegate :image_file_name, :to => :item_base
+  delegate :name, :to => :item_base
 end
