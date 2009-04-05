26511fdf1ec8da94d719bd0ba5410b61338c6816
Daniel X Moore <yahivin@gmail.com>
2009-04-05 00:03:12 +0000

    Fixed nested assignment bug with HAKX again

--- boomhammer/app/controllers/creation/areas_controller.rb
+++ boomhammer/app/controllers/creation/areas_controller.rb
@@ -1,4 +1,11 @@
 class Creation::AreasController < Creation::CreationController
   resource_controller
   actions :all, :except => :destroy
+
+  create.before do
+    # HAX: Nested assignment doesn't link these mofos up
+    @area.opportunities.each do |opportunity|
+      opportunity.area = @area
+    end
+  end
 end
