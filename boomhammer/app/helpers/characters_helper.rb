2783d700c3e1a7cb7a7108b8e63cb937ef534336
Daniel X Moore <yahivin@gmail.com>
2009-04-11 15:53:54 +0000

    Displaying actions remaining on characters

--- boomhammer/app/helpers/characters_helper.rb
+++ boomhammer/app/helpers/characters_helper.rb
@@ -1,2 +1,9 @@
 module CharactersHelper
+  def actions_remaining_for(character)
+    if character.actions > 0
+      "#{character.actions} actions remaining"
+    else
+      "No actions remaining"
+    end
+  end
 end
