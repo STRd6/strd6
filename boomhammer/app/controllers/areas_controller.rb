468332806c6e5b9ae6c35005ebe66242940bdf8f
Daniel X Moore <yahivin@gmail.com>
2009-04-14 05:59:11 +0000

    Character always required for areas

--- boomhammer/app/controllers/areas_controller.rb
+++ boomhammer/app/controllers/areas_controller.rb
@@ -1,7 +1,7 @@
 class AreasController < ResourceController::Base
   actions :show, :index
 
-  before_filter :character_required, :except => [:index]
+  before_filter :character_required
 
   show.before do
     @title = @area.name
