908fb6f524086f34cff7ee9a16b71e5cad299767
Daniel X Moore <yahivin@gmail.com>
2009-04-14 00:26:59 +0000

    Filtering images that show up in selects by size

--- boomhammer/app/helpers/images_helper.rb
+++ boomhammer/app/helpers/images_helper.rb
@@ -19,8 +19,8 @@ module ImagesHelper
     image_tag "/images/badges/#{badge.image_file_name}", :alt => badge, :title => badge
   end
 
-  def image_select(form)
-    Image.all.map do |image|
+  def image_select(form, size=:small)
+    Image.send(size).all.map do |image|
       form.label("image_id_#{image.id}", image_for(image) ) +
       form.radio_button(:image_id, image.id)
     end
