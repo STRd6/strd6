ae635a079440cefa877fa2f54d248b037f52adb3
Daniel X Moore <yahivin@gmail.com>
2009-04-15 05:23:21 +0000

    Added more descriptions and half-assed some eager loading

--- boomhammer/app/controllers/areas_controller.rb
+++ boomhammer/app/controllers/areas_controller.rb
@@ -6,4 +6,14 @@ class AreasController < ResourceController::Base
   show.before do
     @title = @area.name
   end
+
+  protected
+  def object
+    @object ||= Area.find(param, :include => [
+      {:area_base => :image},
+      {:opportunities => {:opportunity_base => [:image, :requisites]}},
+      :shops,
+      {:area_links => [:requisites, :linked_area]}
+    ])
+  end
 end
