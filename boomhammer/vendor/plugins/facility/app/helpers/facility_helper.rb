1f9fb18da3a382b697a4c5b99db88580eb8d61c6
Daniel X Moore <yahivin@gmail.com>
2009-04-17 06:09:27 +0000

    Added form submit helper method

--- boomhammer/vendor/plugins/facility/app/helpers/facility_helper.rb
+++ boomhammer/vendor/plugins/facility/app/helpers/facility_helper.rb
@@ -2,4 +2,8 @@ module FacilityHelper
   def notifications
     render :partial => 'components/notifications'
   end
+
+  def prototype_submit(text="Submit")
+    link_to_function text, "$(this).up('form').submit()"
+  end
 end
