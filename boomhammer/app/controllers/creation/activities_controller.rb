142cc21beb07877c43e577bd06a220f3e6ca658b
Daniel X Moore <yahivin@gmail.com>
2009-04-30 02:53:44 +0000

    Added tabs to activities feed.

--- boomhammer/app/controllers/creation/activities_controller.rb
+++ boomhammer/app/controllers/creation/activities_controller.rb
@@ -1,14 +1,31 @@
 class Creation::ActivitiesController < Creation::CreationController
   resource_controller
+
+  LEGIT = [ItemBase, AreaBase, OpportunityBase, Recipe, Image]
+
   actions :index
+
+  index.wants.html do
+    if params[:spartan]
+      render :partial => "spartan_index"
+    end
+  end
 
   protected
   def collection
-    (
-      AreaBase.all +
-      ItemBase.all +
-      OpportunityBase.all +
-      Recipe.all
-    ).sort_by(&:created_at).reverse
+    entries =
+      if type = params[:type]
+        if (klass = type.constantize) && (LEGIT.include? klass)
+          klass.all
+        end
+      else
+        LEGIT.sum(&:all)
+      end
+
+    if entries
+      entries.sort_by(&:created_at).reverse
+    else
+      []
+    end
   end
 end
