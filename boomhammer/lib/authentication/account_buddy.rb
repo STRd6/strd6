99f99a124f76d2cca59dfe08808f4bd7496ec5d2
Daniel X Moore <yahivin@gmail.com>
2009-05-05 03:44:12 +0000

    Added list of referrals.

--- boomhammer/lib/authentication/account_buddy.rb
+++ boomhammer/lib/authentication/account_buddy.rb
@@ -3,6 +3,7 @@ module Authentication
     def self.included(account)
       account.class_eval do
         has_many :characters
+        has_many :referrals, :class_name => "Account", :foreign_key => "referrer_id"
       end
     end
 
