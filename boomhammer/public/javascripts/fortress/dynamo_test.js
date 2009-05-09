8d073b17eace5d938680069195771eba4ba1b497
Daniel X Moore <yahivin@gmail.com>
2009-05-09 01:10:09 +0000

    Added QUnit testing

---
+++ boomhammer/public/javascripts/fortress/dynamo_test.js
@@ -0,0 +1,17 @@
+(function($) {
+  $(document).ready(function() {
+    test("Random values", function() {
+      var n = rand(2);
+
+      ok(n == 0 || n == 1, "rand(2) gives 0 or 1");
+    });
+
+    test("Random Array Elements", function() {
+      var array = [1,2,3];
+      var emptyArray = [];
+
+      ok(array.indexOf(array.rand()) != -1, "Array includes randomly selected element");
+      ok(emptyArray.rand() == undefined, "Rand on empty array returns undefined");
+    });
+  });
+})(jQuery);
