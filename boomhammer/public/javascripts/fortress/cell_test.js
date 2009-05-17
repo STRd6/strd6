2aa6d47ffebc4b1339f25234f8b19a5b0bee0e17
Daniel X Moore <yahivin@gmail.com>
2009-05-17 22:22:46 +0000

    Removed unrelated test.

--- boomhammer/public/javascripts/fortress/cell_test.js
+++ boomhammer/public/javascripts/fortress/cell_test.js
@@ -20,59 +20,5 @@
       ok(cell2.contents().indexOf(item) != -1, "cell2 should contain item");
       ok(item.container() == cell2, "The contianer of the item should be cell2");
     });
-
-    test("jQueryBug?", function() {
-      var Container = function() {
-        var contents = [];
-
-        var self = {
-          contents: function() {
-            return contents;
-          },
-
-          add: function(object) {
-            return contents.push(object);
-          }
-        };
-
-        return self;
-      };
-
-      var Foo = function() {
-        var self = {};
-        var container;
-
-        var log = function(object) {
-          console.log(object);
-        };
-
-        return $.extend(self, {
-          eachNeighbor: function() {
-            $.each(self.container().contents(), function(i, element) {
-              log(this);
-            });
-          },
-
-          setContainer: function(newContainer) {
-            container = newContainer;
-          },
-
-          container: function() {
-            return container;
-          }
-        });
-
-        return self;
-      }
-
-      var foo = new Foo();
-      var container = Container();
-      container.add({});
-      container.add(foo);
-      foo.setContainer(container);
-
-      foo.eachNeighbor();
-
-    });
   });
 })(jQuery);
