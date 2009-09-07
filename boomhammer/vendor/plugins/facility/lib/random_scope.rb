c69756c74fc39e1a5da64f7f8bca3be3e22f4f70
Daniel X Moore <yahivin@gmail.com>
2009-09-07 19:26:03 +0000

    Added limit parameter to random scope

--- boomhammer/vendor/plugins/facility/lib/random_scope.rb
+++ boomhammer/vendor/plugins/facility/lib/random_scope.rb
@@ -4,9 +4,21 @@ module RandomScope
   def self.included(model)
     model.class_eval do
       if connection.adapter_name == "MySQL"
-        named_scope :random, :order => "RAND()"
+        named_scope :random, lambda { |amount|
+          if(amount)
+            {:order => "RAND()", :limit => amount}
+          else
+            {:order => "RAND()"}
+          end
+        }
       else
-        named_scope :random, :order => "RANDOM()"
+        named_scope :random, lambda { |amount|
+          if(amount)
+            {:order => "RANDOM()", :limit => amount}
+          else
+            {:order => "RANDOM()"}
+          end
+        }
       end
     end
   end
