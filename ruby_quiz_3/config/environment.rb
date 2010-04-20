1bf0fadc48d3922989f4d053370aee1a9a16cc98
Daniel X Moore <yahivin@gmail.com>
2010-04-20 05:30:11 +0000

    Vendored updated rails gems

--- ruby_quiz_3/config/environment.rb
+++ ruby_quiz_3/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -28,7 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "resource_controller"
-  config.gem "haml"
+  config.gem "haml", :version => "2.2.3"
   config.gem "maruku"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
