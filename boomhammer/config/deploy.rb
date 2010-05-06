c2113a14c07dc28dfbe81072f1852cf07e6f357b
Daniel X Moore <yahivin@gmail.com>
2010-05-06 22:06:23 +0000

    Added nginx start tasks to deploy

--- boomhammer/config/deploy.rb
+++ boomhammer/config/deploy.rb
@@ -49,3 +49,19 @@ namespace :juggernaut do
     run "cd #{current_path} && rake juggernaut:restart RAILS_ENV=production"
   end
 end
+
+# Passenger start Tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
